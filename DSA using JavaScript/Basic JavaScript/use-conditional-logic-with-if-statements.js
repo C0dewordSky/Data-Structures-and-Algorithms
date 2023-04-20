df3b758c179e0c00b08332ced54a87b367d9739d
Aakash <aakashsinha1606@gmail.com>
2023-04-21 01:19:33 +0530

    Basics of JavaScript - Reusable JS with Fn and Complete Notes till date

---
+++ DSA using JavaScript/Basic JavaScript/use-conditional-logic-with-if-statements.js
@@ -0,0 +1,12 @@
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue){
+  return "Yes, that was true";              // The code here means that if(true) return this
+}                                      
+return "No, that was false";                // else this . We don't need to use else in such cases because at the end of the function we have to 
+}
+  trueOrFalse(true);
+  trueOrFalse(false);
+
+  // Only change code above this line
+
